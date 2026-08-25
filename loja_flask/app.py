dbba387a3ced8663c097fc6371e80c54b4e45142
rayalima-cell <ray_a_lima@estudante.sesisenai.org.br>
2026-08-25 12:13:32 -0300

    01

--- loja_flask/app.py
+++ loja_flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 #Cria a aplicação Flask
 
 app = Flask(__name__)
@@ -6,7 +6,11 @@ app = Flask(__name__)
 #Define uma rota
 @app.route('/')
 def index():
-    return "Olá, mundo! O Flask OK"
+    return render_template("index.html")
+
+@app.route("/produtos")
+def produtos():
+    return render_template("produtos.html")
 
 @app.route("/sobre")
 def sobre():
